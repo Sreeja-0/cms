2a4aa3ae8f2d1b25bf410e7e2237525cad247070
Sreeja-0 <sreejaippala08@gmail.com>
2026-01-11 13:01:21 +0530

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log('🚀 CMS API running on http://localhost:3000/api');
 }
 bootstrap();
+
